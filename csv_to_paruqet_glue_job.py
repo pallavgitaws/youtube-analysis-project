6e88fa0fa929ae03625768c7e6dbca38787769ac
pallavgitaws <142537777+pallavgitaws@users.noreply.github.com>
2023-08-18 00:57:49 +0530

    Script that has glue job to cleanse csv to parquet

---
+++ csv_to_paruqet_glue_job.py
@@ -0,0 +1,36 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+predicate_pushdown = "region in ('ca','gb','us')"
+
+datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "de_youtube_raw_data", table_name = "raw_statistics", transformation_ctx = "datasource0", push_down_predicate = predicate_pushdown)
+
+applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "bigint", "category_id", "bigint"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "bigint", "views", "bigint"), ("likes", "bigint", "likes", "bigint"), ("dislikes", "bigint", "dislikes", "bigint"), ("comment_count", "bigint", "comment_count", "bigint"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx = "applymapping1")
+
+resolvechoice2 = ResolveChoice.apply(frame = applymapping1, choice = "make_struct", transformation_ctx = "resolvechoice2")
+
+dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ctx = "dropnullfields3")
+
+# Convert the DynamicFrame to a DataFrame for coalesce
+df = dropnullfields3.toDF().coalesce(1)
+
+# Create a new DynamicFrame from the DataFrame
+df_final_output = DynamicFrame.fromDF(df, glueContext, "df_final_output")
+
+# Write the DynamicFrame to S3 with partitioning
+datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://de-on-youtube-cleanseddata-useast1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
+
+job.commit()
